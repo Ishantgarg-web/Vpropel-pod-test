5afc44d50d83a66c002e46fd6725d1952bf6441a
Ishantgarg-web <57170623+Ishantgarg-web@users.noreply.github.com>
2021-05-19 16:22:00 +0530

    19-5-21-Position of Child in Queue Problem

---
+++ Attend-pod-test/Java/86Position of Child in Queue.java
@@ -0,0 +1,13 @@
+// problem link: http://www.vpropel.in/code-test/attend-pod-test/86
+
+public class Main {
+    public static void main(String args[]) {
+        Scanner input=new Scanner(System.in);
+        int x=input.nextInt();
+        int y=input.nextInt();
+        int z=input.nextInt();
+        int n=y+z-1;
+        System.out.println(n);
+        System.out.println(n-x+1);
+    }
+}
